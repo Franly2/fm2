5a1b45e7d3c3ab5300175759fb6bba6ba528ca3c
franly2 <franlybudipramana588@gmail.com>
2026-06-22 21:02:29 +0700

    project-init;tailwind-setup;folder-structure

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-brand-maroon text-4xl mt-10 text-center">
+        Testing Judul Warna Maroon
+      </h1>
+
+      <h2 className="text-center mt-5 text-2xl">
+        Testing Judul Base Layer
+      </h2>
+
+      <p className="text-brand-gray text-center mt-2">
+        Ini adalah teks deskripsi dengan font Montserrat dan warna abu-abu.
+      </p>
+    </div>
+  )
+}
+
+export default App
